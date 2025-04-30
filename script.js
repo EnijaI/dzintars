6a67e18acc6c56db8b4d3189daeea615ee2a5527
EnijaI <igolniecee@gmail.com>
2025-04-30 11:16:00 +0000

    uiyouiouio

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+  const username = document.querySelector('input[name="username"]').value;
+  const password = document.querySelector('input[name="password"]').value;
+
+  if (!username || !password) {
+    event.preventDefault(); // Aptur sūtīšanu
+    alert("Lūdzu, ievadiet gan lietotājvārdu, gan paroli!");
+  }
+});
+window.onload = function() {
+  alert("Laipni lūdzam mūsu mājaslapā!");
+};
+document.querySelector('.links a').addEventListener('click', function(event) {
+  event.preventDefault();
+  alert("Paroles atjaunošanas iespēja vēl nav pieejama.");
+});
+document.querySelector('input[name="remember-me"]').addEventListener('change', function() {
+  if (this.checked) {
+    alert("Tavs lietotājvārds tiks atcerēts!");
+  } else {
+    alert("Tavs lietotājvārds netiks atcerēts.");
+  }
+});
